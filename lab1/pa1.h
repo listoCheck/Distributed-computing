a5ac71677beb20450db9ce3c07b94302a08f8a00
Artem <v.amuzinsky@yandex.ru>
2026-09-14 11:02:45 +0300

    Add lab 1 IPC implementation

---
+++ lab1/pa1.h
@@ -0,0 +1,12 @@
+#ifndef PA1_H
+#define PA1_H
+
+#define events_log "events.log"
+#define pipes_log "pipes.log"
+
+#define log_started_fmt "Process %1d (pid %5d, parent %5d) STARTED\n"
+#define log_received_all_started_fmt "Process %1d received all STARTED messages\n"
+#define log_done_fmt "Process %1d DONE\n"
+#define log_received_all_done_fmt "Process %1d received all DONE messages\n"
+
+#endif
